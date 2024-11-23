e482a29c461f2744e31566030ee97709e724575e
syauqeesy <syauqeesy@gmail.com>
2024-11-23 21:38:20 +0700

    install nvim-tree, kanagawa theme, lsp

--- lua/syauqeesy/packer.lua
+++ lua/syauqeesy/packer.lua
@@ -1,14 +1,22 @@
-vim.cmd [[packadd packer.nvim]]
-
+vim.cmd("packadd packer.nvim")
 return require('packer').startup(function(use)
-  use "wbthomason/packer.nvim"
+    use "wbthomason/packer.nvim"
+
 
   use {
     "nvim-telescope/telescope.nvim", tag = "0.1.8",
     requires = { {"nvim-lua/plenary.nvim"} }
   }
 
-  use("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
+  use "nvim-treesitter/nvim-treesitter"
 
-  use "nvim-treesitter/playground"
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig"
+  }
+
+  use "rebelot/kanagawa.nvim"
+
+  use "nvim-tree/nvim-tree.lua"
 end)
