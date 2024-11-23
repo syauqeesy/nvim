7c12938b8e5bd812c9252e19146ad209f827869a
syauqeesy <syauqeesy@gmail.com>
2024-11-23 21:46:35 +0700

    install typescript lsp

--- lua/syauqeesy/plugin/lsp.lua
+++ lua/syauqeesy/plugin/lsp.lua
@@ -1,7 +1,8 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua-language-server" }
+    ensure_installed = { "lua-language-server", "typescript-language-server" }
 })
 
 require("lspconfig").lua_ls.setup {}
+require("lspconfig").ts_ls.setup {}
 
