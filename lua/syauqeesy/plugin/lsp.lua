50b473b70157b602d161c9d0293e95655667dcce
syauqeesy <syauqeesy@gmail.com>
2024-11-24 09:33:31 +0700

    install gopls and setup formatter

--- lua/syauqeesy/plugin/lsp.lua
+++ lua/syauqeesy/plugin/lsp.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua-language-server", "typescript-language-server" }
+    ensure_installed = { "lua-language-server", "typescript-language-server", "gopls"}
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -10,6 +10,29 @@ require("lspconfig").lua_ls.setup {
 }
 require("lspconfig").ts_ls.setup {
   capabilites = capabilities
+}
+
+require("lspconfig").gopls.setup {
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("GoFormatting", { clear = true }),
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
 }
 
 
--- lua/syauqeesy/remap.lua
+++ lua/syauqeesy/remap.lua
@@ -1,2 +1,7 @@
-# Remap
+vim.api.nvim_set_keymap(
+  'n',
+  'format',
+  '<cmd>lua vim.lsp.buf.format({ async = true })<CR>',
+  { noremap = true, silent = true }
+)
 
