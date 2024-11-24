d6657312f35c27c835008cf5a500d9961c445f51
syauqeesy <syauqeesy@gmail.com>
2024-11-24 17:45:30 +0700

    fix: gitignore, configure lualine & fugitive

---
+++ lua/syauqeesy/plugin/lualine.lua
@@ -0,0 +1,16 @@
+require('lualine').setup {
+  options = {
+    theme = 'auto',
+    section_separators = '',
+    component_separators = ''
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'},
+  },
+}
+
